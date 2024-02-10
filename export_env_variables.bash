226c490dd685c864d8e5a3f66f863532f7eba6d3
sfeng65 <sfeng65@gatech.edu>
2024-02-10 00:42:19 -0500

    modify env variables sh

--- export_env_variables.bash
+++ export_env_variables.bash
@@ -4,7 +4,7 @@ echo ${GF_SRC_PREFIX}
 export GF_ORB_SLAM2_SDK=${GF_SRC_PREFIX}/good_feature/SDK/
 
 # set dependencies install dir, all 3rdparty libs will be installed here
-[[ -z ${SLAM_OPENSOURCE_ROOT} ]] && GF_LIB_PREFIX="/opt" || GF_LIB_PREFIX=${SLAM_OPENSOURCE_ROOT}
+[[ -z ${GF_SLAM_OPENSOURCE_ROOT} ]] && GF_LIB_PREFIX="/opt" || GF_LIB_PREFIX=${GF_SLAM_OPENSOURCE_ROOT}
 echo ${GF_LIB_PREFIX}
 export GF_ORB_SLAM2_ROOT=${GF_LIB_PREFIX}/gf_orb_slam2
 # export GF_ORB_SLAM2_ROOT=/opt/gf_orb_slam2
